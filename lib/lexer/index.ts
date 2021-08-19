212f360e83d16c09e96cc492d40b353f854c8527
Sergei Zharinov <sergio.zharinov@gmail.com>
2021-08-19 13:17:16 +0300

    Add moo iterator wrapper

--- lib/lexer/index.ts
+++ lib/lexer/index.ts
@@ -1,3 +1,5 @@
+import type { Token as MooToken } from 'moo';
+import { states as mooStates } from 'moo';
 import type { BracketOption } from './bracket';
 import { configBrackets } from './bracket';
 import type { CommentOption } from './comment';
@@ -9,6 +11,7 @@ import { fallbackRule } from './rules';
 import type { StringOption } from './string';
 import { configStrings } from './string';
 import { configSymbols } from './symbol';
+import { coerceToken, Token } from './token';
 
 interface LexerConfig {
   comments: CommentOption[];
@@ -18,7 +21,7 @@ interface LexerConfig {
   strings: StringOption[];
 }
 
-export function configureLexer(config: LexerConfig): StatesMap {
+export function configureLexerRules(lexerConfig: LexerConfig): StatesMap {
   let result: StatesMap = {
     $: {
       newline: { t: 'regex', match: /\r?\n/, lineBreaks: true },
@@ -26,12 +29,50 @@ export function configureLexer(config: LexerConfig): StatesMap {
       ___: fallbackRule,
     },
   };
+  const { comments, symbols, operators, brackets, strings } = lexerConfig;
+  result = configComments(result, comments);
+  result = configSymbols(result, { match: symbols });
+  result = configOperators(result, operators);
+  result = configBrackets(result, brackets);
+  result = configStrings(result, strings);
+  return result;
+}
 
-  result = configComments(result, config.comments);
-  result = configSymbols(result, { match: config.symbols });
-  result = configOperators(result, config.operators);
-  result = configBrackets(result, config.brackets);
-  result = configStrings(result, config.strings);
+export interface Lexer {
+  reset(input?: string): Lexer;
+  [Symbol.iterator](): Iterator<Token, null>;
+}
+
+export function createLexer(options: LexerConfig): Lexer {
+  const rules = configureLexerRules(options);
+  const mooLexer = mooStates(rules);
+
+  const result: Lexer = {
+    reset(input?: string) {
+      if (typeof input === 'undefined' || input === null) {
+        mooLexer.reset();
+      } else {
+        mooLexer.reset(input);
+      }
+      return result;
+    },
+
+    [Symbol.iterator]() {
+      const mooIter = mooLexer[Symbol.iterator]();
+
+      const next = (): IteratorResult<Token, null> => {
+        const nextElem = mooIter.next();
+        if (nextElem.done) {
+          return { done: true, value: null };
+        } else {
+          const value = coerceToken(nextElem.value);
+          return { done: false, value };
+        }
+      };
+
+      return { next };
+    },
+  };
 
   return result;
 }
