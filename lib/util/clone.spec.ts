95955a9b9136f95a11098da36e92b06c6bdc037e
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-05-12 14:55:48 +0300

    fix(util/clone): Use native `structuredClone` for deep copying

---
+++ lib/util/clone.spec.ts
@@ -0,0 +1,67 @@
+import { clone } from './clone';
+
+describe('util/clone', () => {
+  describe('primitives', () => {
+    it.each`
+      value
+      ${1}
+      ${'a'}
+      ${true}
+      ${false}
+      ${null}
+      ${undefined}
+    `('$value -> $value', ({ value }) => {
+      const cloned = clone<unknown>(value);
+      expect(cloned).toBe(value);
+    });
+  });
+
+  describe('non-primitives', () => {
+    it.each`
+      from              | to
+      ${[]}             | ${[]}
+      ${[1, 2, 3]}      | ${[1, 2, 3]}
+      ${{}}             | ${{}}
+      ${{ a: 1, b: 2 }} | ${{ a: 1, b: 2 }}
+    `('$from -> $to', ({ from, to }) => {
+      const cloned = clone<unknown>(from);
+      expect(cloned).toEqual(to);
+      expect(cloned).not.toBe(from);
+    });
+
+    test('classes', () => {
+      class Test {
+        constructor(private data: { value: number }) {}
+
+        getData(): { value: number } {
+          return this.data;
+        }
+      }
+
+      const a = new Test({ value: 42 });
+      const b = clone(a);
+
+      expect(b).toEqual(a);
+      expect(b).not.toBe(a);
+      expect(b.getData()).toEqual(a.getData());
+      expect(b.getData()).not.toBe(a.getData());
+    });
+
+    test('circular references', () => {
+      type Test = { value: number; self?: Test };
+      const a: Test = { value: 42 };
+      a.self = a;
+
+      const b = clone(a);
+
+      expect(b).toEqual(a);
+      expect(b).not.toBe(a);
+      expect(b.self).toBe(b);
+
+      expect(b).not.toBe(a);
+      expect(b).not.toBe(a.self);
+      expect(b.self).not.toBe(a.self);
+      expect(b.self).not.toBe(a);
+    });
+  });
+});
